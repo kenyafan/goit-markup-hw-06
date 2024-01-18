c3e4e6b3078de5c6a202cb1f6c83a8dc136aa962
kenyafan <kenyafan321@gmail.com>
2024-01-18 20:54:11 +0200

    css

---
+++ Js/main.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const reviewsBtn = document.querySelector('.button');
+  const modalCloseBtn = document.querySelector('.close-btn');
+  const backdrop = document.querySelector('.backdrop');
+
+  reviewsBtn.addEventListener('click', function() {
+    backdrop.classList.add('is-open');
+  });
+
+  modalCloseBtn.addEventListener('click', function() {
+    backdrop.classList.remove('is-open');
+  });
+});
+
+const menuBtn = document.querySelector('.mobil-menu-btn'); 
+const mobileCloseBtn = document.querySelector('.close-btn.mobile-close');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuBtn.addEventListener('click', function() {
+  mobileMenu.classList.add('is-open-mobile');
+});
+
+mobileCloseBtn.addEventListener('click', function() {
+  mobileMenu.classList.remove('is-open-mobile');
+});
